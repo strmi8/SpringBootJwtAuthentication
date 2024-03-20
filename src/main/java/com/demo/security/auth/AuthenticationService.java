03979846694c4e3c20f3cc0a11aed62d29adf392
Stjepan Strmečki <stjepan.strmecki@fina.hr>
2024-03-20 13:41:25 +0100

    Created a security project using jwt authentication.

---
+++ src/main/java/com/demo/security/auth/AuthenticationService.java
@@ -0,0 +1,59 @@
+package com.demo.security.auth;
+
+import com.demo.security.config.JwtService;
+import com.demo.security.user.Role;
+import com.demo.security.user.User;
+import com.demo.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+
+        var jwToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse
+                .builder()
+                .token(jwToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        var jwToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse
+                .builder()
+                .token(jwToken)
+                .build();
+    }
+}
